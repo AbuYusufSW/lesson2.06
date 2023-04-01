e9fdbc2150a668c6fcdb5f502115746965fbd6ba
AbuYusuf <AbuYusufSW@gmail.com>
2023-04-01 13:10:37 +0300

    Initial Commit

---
+++ lesson2.06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson2.06
+//
+//  Created by AbuYusuf on 01.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
